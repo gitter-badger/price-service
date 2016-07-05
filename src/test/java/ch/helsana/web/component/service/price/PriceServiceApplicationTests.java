9dd62766581e209c0ce4c2c92824b0ff6fb76041
Marcel Widmer <marzelwidmer@gmail.com>
2016-07-05 07:51:33 +0200

    add some Junit stuff

--- src/test/java/ch/helsana/web/component/service/price/PriceServiceApplicationTests.java
+++ src/test/java/ch/helsana/web/component/service/price/PriceServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package ch.helsana.web.component.service.price;
 
+import ch.helsana.web.component.service.price.service.PriceService;
+import ch.sbi.services.system.productengine.tarif.v2.BerechnePraemieBusinessFaultMessage;
+import ch.sbi.services.system.productengine.tarif.v2.BerechnePraemieSystemFaultMessage;
+import ch.sbi.services.system.productengine.tarif.v2.berechnepraemierequest.BerechnePraemieRequest;
+import ch.sbi.services.system.productengine.tarif.v2.berechnepraemierequest.Person;
+import ch.sbi.services.system.productengine.tarif.v2.berechnepraemierequest.PersonListType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,10 +22,19 @@ public class PriceServiceApplicationTests {
 
 
 	@Autowired
-	Environment env;
+	PriceService service;
+
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws BerechnePraemieBusinessFaultMessage, BerechnePraemieSystemFaultMessage {
+		BerechnePraemieRequest request = new BerechnePraemieRequest();
+		request.setCorrelationId("42342");
+		PersonListType personListType = new PersonListType();
+		Person person = new Person();
+		person.setGeschlecht("m");
+		personListType.getPerson().add(person);
+		request.setPersonList(personListType);
+		service.berechnePraemie(request);
 
 	}
 
