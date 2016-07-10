29c1aaa94694cb23e2cf176977a513bf7f99a75b
Marcel Widmer <marzelwidmer@gmail.com>
2016-07-10 20:24:19 +0200

    Update entry API site add product as resources add some dummy bootstrap data in h2 db

--- src/main/java/ch/helsana/web/priceservice/api/IndexController.java
+++ src/main/java/ch/helsana/web/priceservice/api/IndexController.java
@@ -1,6 +1,7 @@
 package ch.helsana.web.priceservice.api;
 
 
+import ch.helsana.web.priceservice.api.product.ProductController;
 import ch.helsana.web.priceservice.controller.PriceServiceController;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpEntity;
@@ -29,8 +30,13 @@ public class IndexController {
 
         List<Link> links = new ArrayList<Link>();
         Link self = linkTo(IndexController.class).withSelfRel();
+
+        Link products = linkTo(methodOn(ProductController.class).showAll()).withRel("products");
+        //Link products = linkTo(ProductController.class).withRel("products");
+
         Link productBestPrice = linkTo(methodOn(PriceServiceController.class).berechneBesterPreis(null)).withRel("best-price");
         Link productPrice = linkTo(methodOn(PriceServiceController.class).berechnePraemie(null)).withRel("price");
+
 
 
        /* Link person = linkTo(methodOn(PersonController.class).show(1L)).withRel("person");
@@ -41,6 +47,7 @@ public class IndexController {
         Link search = linkTo(methodOn(PersonController.class).searchPersonForm()).withRel("search");
 */
         links.add(self);
+        links.add(products);
         links.add(productBestPrice);
         links.add(productPrice);
         //links.add(search);
---
+++ src/main/java/ch/helsana/web/priceservice/api/product/ProductResourceAssembler.java
@@ -0,0 +1,37 @@
+package ch.helsana.web.priceservice.api.product;
+
+import ch.helsana.web.priceservice.model.Product;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by marcelwidmer on 10/07/16.
+ */
+@Component
+public class ProductResourceAssembler extends ResourceAssemblerSupport<Product, ProductResource> {
+    /**
+     * Constructor
+     */
+    public ProductResourceAssembler() {
+        super(ProductController.class, ProductResource.class);
+    }
+
+    /**
+     * Convert domain product to resource product
+     *
+     * @param product
+     * @return
+     */
+    @Override
+    public ProductResource toResource(Product product) {
+        ProductResource productResource = createResourceWithId(product.getId(), product); // adds a "self" link
+
+        // TODO: copy properties from product to productResource
+        productResource.setPrice(product.getPrice());
+        productResource.setImageUrl(product.getImageUrl());
+        productResource.setProductId(product.getProductId());
+        productResource.setDescription(product.getDescription());
+        return productResource;
+    }
+}
+
