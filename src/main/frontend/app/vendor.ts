e32f25926408a52be7e739fc78df3ed88a5620cb
mtinner <m1tinner@gmail.com>
2016-07-14 08:57:21 +0200

    restructure Prod

--- src/main/frontend/app/vendor.ts
+++ src/main/frontend/app/vendor.ts
@@ -1,15 +1,15 @@
-// Angular 2
-import '@angular/common/index.js';
-import '@angular/compiler/index.js';
-import '@angular/core/index.js';
-import '@angular/forms/index.js';
-import '@angular/http/index.js';
-import '@angular/platform-browser/index.js';
-import '@angular/platform-browser-dynamic/index.js';
-import '@angular/upgrade/index.js';
+ // Angular 2
+ import '@angular/common';
+ import '@angular/compiler';
+ import '@angular/core';
+ import '@angular/forms';
+ import '@angular/http';
+ import '@angular/platform-browser';
+ import '@angular/platform-browser-dynamic';
+ import '@angular/upgrade';
 
-// RxJS
-import 'rxjs';
+ // RxJS
+ import 'rxjs';
 
-// Other vendors for example jQuery, Lodash or Bootstrap
-// You can import js, ts, css, sass, ...
+ // Other vendors for example jQuery, Lodash or Bootstrap
+ // You can import js, ts, css, sass, ...
--- src/main/frontend/webpack.prod.js
+++ src/main/frontend/webpack.prod.js
@@ -2,16 +2,24 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map', //--> source-map for prod
 
     output: {
-        filename: '[name].js',
+        publicPath: '',
+        filename: 'app/[name].js',
         chunkFilename: '[id].chunk.js'
     },
 
+    htmlLoader: {
+        minimize: false // workaround for ng2
+    },
+
     plugins: [
-        new ExtractTextPlugin('[name].css')
+        new ExtractTextPlugin('app/[name].css')
     ],
 
     devServer: {
