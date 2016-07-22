88601e8c048e6dc08ec5c0f14c057c8ec87b8bd5
Marcel Tinner <m1tinner@gmail.com>
2016-07-22 12:35:37 +0200

    change doublequotes to singlequotes

--- src/main/frontend/gulp/browserify.js
+++ src/main/frontend/gulp/browserify.js
@@ -1,20 +1,20 @@
 module.exports = function (gulp, data, util, taskName) {
-    var browserify = require("browserify"),
-        babelify = require("babelify"),
+    var browserify = require('browserify'),
+        babelify = require('babelify'),
         source = require('vinyl-source-stream');
 
     gulp.task(taskName + ':Dev', function () {
 
         var input = browserify({debug: true})
-            .transform("babelify", {presets: ["es2015"]})
-            .require(data.path.FRONTEND + "app/scripts/mainInput.js", {entry: true})
+            .transform('babelify', {presets: ['es2015']})
+            .require(data.path.FRONTEND + 'app/scripts/mainInput.js', {entry: true})
             .bundle()
             .pipe(source('bundleInput.js'))
             .pipe(gulp.dest(data.path.DEV + 'app/scripts'));
 
         var price = browserify({debug: true})
-            .transform("babelify", {presets: ["es2015"]})
-            .require(data.path.FRONTEND + "app/scripts/mainPrice.js", {entry: true})
+            .transform('babelify', {presets: ['es2015']})
+            .require(data.path.FRONTEND + 'app/scripts/mainPrice.js', {entry: true})
             .bundle()
             .pipe(source('bundlePrice.js'))
             .pipe(gulp.dest(data.path.DEV + 'app/scripts'));
@@ -25,15 +25,15 @@ module.exports = function (gulp, data, util, taskName) {
 
     gulp.task(taskName + ':Prod', function () {
         var input = browserify({debug: true})
-            .transform("babelify", {presets: ["es2015"]})
-            .require(data.path.FRONTEND + "app/scripts/mainInput.js", {entry: true})
+            .transform('babelify', {presets: ['es2015']})
+            .require(data.path.FRONTEND + 'app/scripts/mainInput.js', {entry: true})
             .bundle()
             .pipe(source('bundleInput.js'))
             .pipe(gulp.dest(data.path.PROD + 'app/scripts'));
 
         var price = browserify({debug: true})
-            .transform("babelify", {presets: ["es2015"]})
-            .require(data.path.FRONTEND + "app/scripts/mainPrice.js", {entry: true})
+            .transform('babelify', {presets: ['es2015']})
+            .require(data.path.FRONTEND + 'app/scripts/mainPrice.js', {entry: true})
             .bundle()
             .pipe(source('bundlePrice.js'))
             .pipe(gulp.dest(data.path.PROD + 'app/scripts'));
