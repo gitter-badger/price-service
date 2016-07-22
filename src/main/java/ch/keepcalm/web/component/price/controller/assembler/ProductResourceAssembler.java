2d18dd9d08b4100624c5bce778f736bd182ab814
Marcel Widmer <marzelwidmer@gmail.com>
2016-07-22 22:10:10 +0200

    Work on Links

--- src/main/java/ch/keepcalm/web/component/price/controller/assembler/ProductResourceAssembler.java
+++ src/main/java/ch/keepcalm/web/component/price/controller/assembler/ProductResourceAssembler.java
@@ -1,6 +1,6 @@
 package ch.keepcalm.web.component.price.controller.assembler;
 
-import ch.keepcalm.web.component.price.controller.ProductController;
+import ch.keepcalm.web.component.price.controller.CustomerController;
 import ch.keepcalm.web.component.price.model.Product;
 import ch.keepcalm.web.component.price.resource.ProductResource;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
@@ -10,16 +10,37 @@ import org.springframework.stereotype.Component;
 public class ProductResourceAssembler extends ResourceAssemblerSupport<Product, ProductResource> {
 
     public ProductResourceAssembler() {
-        super(ProductController.class, ProductResource.class);
+        super(CustomerController.class, ProductResource.class);
     }
 
     @Override
     public ProductResource toResource(Product product) {
-        // api/products>;rel="self"
+        // api/customer/5>;rel="self" // TODO: 22/07/16 missing products/{id} 
         ProductResource productResource = createResourceWithId(product.getId(), product);
         productResource.setProduct(product);
+/*
+
+        // api/customers/1/products/1
+       Link selfLink = new Link(linkTo(CustomerAggregateController.class)
+                .slash("products").slash(product.getId()).toUriComponentsBuilder().build().toUriString(), "self");
+        productResource.add(selfLink);
+
+        Link selfLink1 =(linkTo(methodOn(CustomerAggregateController.class).getProductFromCustomer(id, product.getId())).withSelfRel());
+        productResource.add(selfLink1);
+
+       Link updateProductPriceLink = new Link(linkTo(CustomerAggregateController.class)
+                .slash(product.getId())
+                .slash("products").slash(product.getId()).toUriComponentsBuilder().build().toUriString(), "update_price");
+        productResource.add(updateProductPriceLink);
+        Link selfLink1 = (linkTo(methodOn(CustomerAggregateController.class).getProductsFromCustomer(0)).withSelfRel());
+
+        Link slef = productResource.getLink("slef");
+        Link updateProductPriceLink = new Link(linkTo(CustomerAggregateController.class)
+                .slash(product.getId())
+                .slash("products").slash(product.getId()).toUriComponentsBuilder().build().toUriString(), "update_price");
+
+*/
         return productResource;
     }
-
 
 }
