4c3d75ac5066f937869642283eaa7da516e33a48
Marcel Tinner <m1tinner@gmail.com>
2016-07-25 07:41:56 +0200

    fake backend

--- src/main/frontend/app/scripts/inputService.js
+++ src/main/frontend/app/scripts/inputService.js
@@ -1,20 +1,82 @@
 export const inputService = (function () {
-    var links = {};
+    let links = {};
+
+    let getApi = {
+        "_links": {
+            "customers": {
+                "href": "http://localhost:8080/api/customers"
+            },
+            "self": {
+                "href": "http://localhost:8080/api"
+            }
+        }
+    };
+
+    let getCustomers = {
+        "customerResourceList": [],
+        "_links": {
+            "self": {
+                "href": "http://localhost:8080/api/customers"
+            },
+            "create_customer": {
+                "href": "http://localhost:8080/api/customers"
+            },
+            "list_customers": {
+                "href": "http://localhost:8080/api/customers"
+            }
+        }
+    };
+
+    let postCustomer = {
+        "dateOfBirth": "1990-09-09T00:00:00.000+0000",
+        "gender": "m",
+        "address": {
+            "municipality": "Dübendorf",
+            "municipality_nr": "191",
+            "postal_code": "8044",
+            "postal_code_addition": "00",
+            "locality": "Gockhausen"
+        },
+        "_links": {
+            "self": {
+                "href": "http://localhost:8080/api/customers/1"
+            },
+            "list_products": {
+                "href": "http://localhost:8080/api/customers/1/products"
+            },
+            "create_product": {
+                "href": "http://localhost:8080/api/customers/1/products"
+            },
+            "create_deeplink": {
+                "href": "http://localhost:8080/api/customers/1/deeplink"
+            }
+        }
+    };
 
     (function () {
         return new Promise(function (resolve, reject) {
-            $.getJSON('api', {})
-                .done(function (data) {
-                    setLinks(data);
-                    customer()
-                        .then(function (response) {
-                            setLinks(response);
-                            resolve(response);
-                        });
-                })
-                .fail(function () {
-                    reject('Failed to get API');
-                })
+            setLinks(getApi);
+            console.log('got API');
+            customer()
+                .then(
+                    function (response) {
+                        resolve(response);
+                    }
+                );
+
+
+            /*$.getJSON('api', {})
+             .done(function (data) {
+             setLinks(data);
+             customer()
+             .then(function (response) {
+             setLinks(response);
+             resolve(response);
+             });
+             })
+             .fail(function () {
+             reject('Failed to get API');
+             })*/
         });
     })();
 
@@ -25,19 +87,21 @@ export const inputService = (function () {
     function customer() {
         return new Promise(function (resolve, reject) {
             if (links.customers) {
+                setLinks(getCustomers);
+                console.log('got Customer');
+                resolve(getCustomers);
+                /*$.getJSON(links.customers.href, {})
+                 .done(success)
+                 .fail(fail);
 
-                $.getJSON(links.customers.href, {})
-                    .done(success)
-                    .fail(fail);
+                 function success(response) {
+                 setLinks(response);
+                 resolve(response);
+                 }
 
-                function success(response) {
-                    setLinks(response);
-                    resolve(response);
-                }
-
-                function fail() {
-                    reject('Failed to get Customer');
-                }
+                 function fail() {
+                 reject('Failed to get Customer');
+                 }*/
             } else {
                 resolve('Clould not get Customer');
             }
@@ -49,17 +113,21 @@ export const inputService = (function () {
 
             if (links.create_customer) {
 
-                postHelper(links.create_customer.href, obj)
-                    .then(success, fail);
+                setLinks(postCustomer);
+                console.log('customer created');
+                resolve(postCustomer);
 
-                function success(response) {
-                    setLinks(response);
-                    resolve(response);
-                }
+                /*   postHelper(links.create_customer.href, obj)
+                 .then(success, fail);
 
-                function fail() {
-                    reject('Failed to create Customer');
-                }
+                 function success(response) {
+                 setLinks(response);
+                 resolve(response);
+                 }
+
+                 function fail() {
+                 reject('Failed to create Customer');
+                 }*/
             } else {
                 resolve('Clould not create Customer');
             }
@@ -80,4 +148,5 @@ export const inputService = (function () {
             links = response._links;
         }
     }
+
 })();
--- src/main/frontend/app/scripts/priceService.js
+++ src/main/frontend/app/scripts/priceService.js
@@ -1,6 +1,37 @@
 export const priceService = (function () {
     var links = {};
 
+    let postProduct = {
+        "description": null,
+        "productNumber": "PRO_x0BAS__HEL_IG",
+        "unfall": "COD_ausgeschlossen_HEL",
+        "franchise": "COD_Franchise_KVG-O_Erwachsener_2500_HEL",
+        "drittesKind": null,
+        "price": 0,
+        "_links": {
+            "self": {
+                "href": "http://localhost:8080/api/products/0"
+            },
+            "update_price": {
+                "href": "http://localhost:8080/api/products/0"
+            }
+        }
+    };
+
+    let getProductPrice = {
+        "description": null,
+        "productNumber": "PRO_x0BAS__HEL_IG",
+        "unfall": "COD_ausgeschlossen_HEL",
+        "franchise": "COD_Franchise_KVG-O_Erwachsener_2500_HEL",
+        "drittesKind": null,
+        "price": 278.50,
+        "_links": {
+            "self": {
+                "href": "http://localhost:8080/api/products/0"
+            }
+        }
+    };
+
     return {
         createProduct: createProduct,
         getPrice: getPrice,
@@ -12,18 +43,19 @@ export const priceService = (function () {
         return new Promise(function (resolve, reject) {
             if (links.create_product) {
 
-                postHelper(links.create_product.href, obj)
-                    .then(success, fail);
+                resolve(postProduct);
+                /*   postHelper(links.create_product.href, obj)
+                 .then(success, fail);
 
-                function success(response) {
-                    //do not navigate into product
-                    //setLinks(response);
-                    resolve(response);
-                }
+                 function success(response) {
+                 //do not navigate into product
+                 //setLinks(response);
+                 resolve(response);
+                 }
 
-                function fail() {
-                    reject('Failed to create Product');
-                }
+                 function fail() {
+                 reject('Failed to create Product');
+                 }*/
 
             } else {
                 resolve('could not create Product');
@@ -33,20 +65,22 @@ export const priceService = (function () {
 
     function getPrice() {
         return new Promise(function (resolve, reject) {
+            resolve(getProductPrice);
             if (links.update_price) {
 
-                postHelper(links.update_price.href, obj)
-                    .then(success, fail);
 
-                function success(response) {
-                    //do not navigate into product
-                    //setLinks(response);
-                    resolve(response);
-                }
+                /*     postHelper(links.update_price.href, obj)
+                 .then(success, fail);
 
-                function fail() {
-                    reject('Failed to get Product');
-                }
+                 function success(response) {
+                 //do not navigate into product
+                 //setLinks(response);
+                 resolve(response);
+                 }
+
+                 function fail() {
+                 reject('Failed to get Product');
+                 }*/
 
             } else {
                 resolve('could not get Price Product');
@@ -69,4 +103,5 @@ export const priceService = (function () {
             links = response._links;
         }
     }
+
 })();
