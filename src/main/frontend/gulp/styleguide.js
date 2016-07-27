79e13aeae1996c254fb725e260cd5e5cd03c3e3d
Marcel Tinner <m1tinner@gmail.com>
2016-07-27 07:41:42 +0200

    fix styleguide icons

--- src/main/frontend/gulp/styleguide.js
+++ src/main/frontend/gulp/styleguide.js
@@ -3,20 +3,20 @@ module.exports = function (gulp, data, util, taskName) {
     var postcss = require('gulp-postcss');
 
     gulp.task(taskName, function () {
-        return gulp.src(data.path.STYLEGUIDE + 'app/all.css').pipe( // any files have to existing
+        return gulp.src(data.path.STYLEGUIDE + 'app/inputAsset.css').pipe( // any files have to existing
             postcss([
                 require('mdcss')({
                     theme: require('mdcss-theme-engage')({
                         logo: 'app/images/icon.png',
                         examples: {
-                            css: ['./all.css']
+                            css: ['./app/styles/inputAsset.css']
                         }
                     }),
                     destination: data.path.STYLEGUIDE // to create mdcss
                 })
             ])
         ).pipe(
-            gulp.dest(data.path.STYLEGUIDE) // dest des styles
+            gulp.dest(data.path.STYLEGUIDE + 'app/styles') // dest des styles
         );
     });
 };
